5316bf8f40b8875aa2aff64781546c1f734f18bb
TheGameFather <f15gdsy@gmail.com>
2014-12-15 23:06:04 +0800

    - Init

---
+++ Core/ModelLoader.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+namespace RuntimeModelLoader {
+
+	public class ModelLoader {
+
+		public static Mesh Load (string path) {
+			if (!File.Exists(path)) return null;
+
+			string[] parts = path.Split(new []{"."}, System.StringSplitOptions.RemoveEmptyEntries);
+			if (parts.Length < 2) {	// No file extension
+				return null;
+			}
+
+			ParserBase parser = null;
+
+			string extention = parts[1];
+			switch (extention) {
+			case "obj":
+				parser = new ObjParser();
+				break;
+			}
+
+			Mesh mesh = parser.Parse(path);
+
+			return mesh;
+		}
+	}
+
+}
---
+++ Ex/ModelLoaderEx.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RuntimeModelLoader;
+
+namespace RuntimeModelLoaderEx {
+
+	public class ModelLoaderEx {
+
+		public static GameObject LoadModelGameObject (string path) {
+			RuntimeModelLoader.Mesh runtimeMesh = ModelLoader.Load(path);
+			if (runtimeMesh == null) {return null;}
+
+			List<Vector3> vertices = Converter.GetVerticesVector3(runtimeMesh);
+			List<int> triangles = Converter.GetTriangules(runtimeMesh);
+
+			GameObject go = new GameObject();
+
+			UnityEngine.Mesh goMesh = new UnityEngine.Mesh();
+			goMesh.vertices = vertices.ToArray();
+			goMesh.triangles = triangles.ToArray();
+
+			MeshFilter meshFilter = go.AddComponent<MeshFilter>();
+			meshFilter.mesh = goMesh;
+
+			go.AddComponent<MeshRenderer>();
+
+			return go;
+		}
+	}
+
+
+	public static class Converter {
+		public static Vector3 Vec3ToVector3 (Vec3 vec3) {
+			return new Vector3(vec3.x, vec3.y, vec3.z);
+		}
+
+		public static List<Vector3> GetVerticesVector3 (RuntimeModelLoader.Mesh mesh) {
+			List<Vector3> vertices = new List<Vector3>();
+			foreach (Vec3 vec3 in mesh.vertices) {
+				Vector3 vector3 = Converter.Vec3ToVector3(vec3);
+				vertices.Add(vector3);
+			}
+			return vertices;
+		}
+
+		public static List<int> GetTriangules (RuntimeModelLoader.Mesh mesh) {
+			List<int> triangles = new List<int>();
+			foreach (Face face in mesh.faces) {
+				triangles.Add(face.vertexIndices[0]);
+				triangles.Add(face.vertexIndices[1]);
+				triangles.Add(face.vertexIndices[2]);
+			}
+			return triangles;
+		}
+	}
+}
